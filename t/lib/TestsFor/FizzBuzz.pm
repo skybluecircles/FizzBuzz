ce0c273def40f427fb0034f0e3d027e76d0a2ff8
William Stevenson <wds@cpan.org>
2014-08-27 14:28:30 -0400

    TestsFor FizzBuzz

---
+++ t/lib/TestsFor/FizzBuzz.pm
@@ -0,0 +1,86 @@
+package TestsFor::FizzBuzz;
+
+use strict;
+use warnings;
+
+use v5.15;
+
+use autodie qw(:all);
+use feature qw(say);
+
+use FizzBuzz;
+
+use Test::Class::Moose;
+use Test::Fatal;
+
+sub test_valid {
+    my @tests = (
+        {   params   => { fizz => 3, buzz => 5, start => 1, stop => 16, },
+            expected => [
+                qw/
+                    1
+                    2
+                    Fizz
+                    4
+                    Buzz
+                    Fizz
+                    7
+                    8
+                    Fizz
+                    Buzz
+                    11
+                    Fizz
+                    13
+                    14
+                    FizzBuzz
+                    16
+                    /
+            ],
+        },
+        {   params   => { fizz => 2, buzz => 3, start => 1, stop => 16, },
+            expected => [
+                qw/
+                    1
+                    Fizz
+                    Buzz
+                    Fizz
+                    5
+                    FizzBuzz
+                    7
+                    Fizz
+                    Buzz
+                    Fizz
+                    11
+                    FizzBuzz
+                    13
+                    Fizz
+                    Buzz
+                    Fizz
+                    /
+            ],
+        },
+    );
+
+    foreach my $test ( @tests ) {
+        is_deeply( FizzBuzz->new( $test->{params} )->series(),
+            $test->{expected} );
+    }
+}
+
+sub test_invalid {
+    my @tests = (
+        {   params => { fizz => 2, buzz => 2, start => 1, stop => 16, },
+            expected => qr/^fizz and buzz must be different/,
+        },
+        {   params => { fizz => 2, buzz => 5, start => 16, stop => 1, },
+            expected => qr/^stop must be less than start/,
+        },
+    );
+
+    foreach my $test ( @tests ) {
+        like( exception { FizzBuzz->new( $test->{params} ) },
+            $test->{expected} );
+    }
+}
+
+1;
