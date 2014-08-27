5d1f05dfeb2f1aa6ed3db0335794999c5077472b
William Stevenson <wds@cpan.org>
2014-08-27 12:39:19 -0400

    Used new FizzBuzz module in fizzbuzz script

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -8,81 +8,8 @@ use v5.16;
 use autodie qw(:all);
 use feature qw(say);
 
-my $a = 3;
-my $b = 5;
-my $c = $a * $b;
+use FizzBuzz;
 
-for ( 1 .. 100 ) {
-    fizzbuzz($_);
-}
+FizzBuzz->new_with_options()->print();
 
 exit 0;
-
-sub fizzbuzz {
-    my $num = shift;
-
-    if ( $num % $c == 0 ) {
-        say 'FizzBuzz';
-    }
-    elsif ( $num % $b == 0 ) {
-        say 'Buzz';
-    }
-    elsif ( $num % $a == 0 ) {
-        say 'Fizz';
-    }
-    else {
-        say $num;
-    } 
-}
-
-=pod
-
-=head1 NAME
-
-FizzBuzz
-
-=head1 SYNOPSIS
-
-  $ fizzbuzz
-
-=head1 DESCRIPTION
-
-Takes no arguments.
-
-Assumes two numbers: 3 and 5.
-
-For the numbers 1 to 100:
-
-=over 4
-
-=item
-
-if the number is divisible by both 3 and 5, prints 'FizzBuzz'
-
-=item
-
-if the number is divisible by 5 (and not by 3),  prints 'Buzz'
-
-=item
-
-if the number is divisible by 3 (and not by 5),  prints 'Fizz'
-
-=item
-
-otherwise, prints the number
-
-=back
-
-=head1 AUTHOR
-
-William Stevenson <william@skybluecircles.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2014 by William Stevenson.
-
-This is free software, licensed under:
-
-    The GNU Public License version 3.0
-
-=cut
